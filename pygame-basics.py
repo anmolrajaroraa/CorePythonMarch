35b9a280aee4cccf0947306929ad0e3aca7b4a69
Anmol Arora <anmolarora1711@gmail.com>
2020-04-25 14:56:03 +0530

    Code

--- pygame-basics.py
+++ pygame-basics.py
@@ -2,7 +2,10 @@ import pygame
 import random
 pygame.init()
 
-screen = pygame.display.set_mode((1000, 500))
+width = 900
+height = 500
+
+screen = pygame.display.set_mode((width, height))
 
 # rgb format
 # primary colours - red green blue
@@ -13,74 +16,117 @@ red = 255, 0, 0
 green = 0, 255, 0
 blue = 0, 0, 255
 screen.fill(white)
-x = 100
-y = 100
-moveX = 0
-moveY = 0
-x2 = random.randint(0, 950)
-y2 = random.randint(0, 450)
+# width = 50
 
-while True:
-    for event in pygame.event.get():
-        # print(event)
-        if event.type == pygame.QUIT:
-            quit()
-            pygame.quit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                moveX = 1
-                moveY = 0
-            elif event.key == pygame.K_DOWN:
-                moveY = 1
-                moveX = 0
-            elif event.key == pygame.K_LEFT:
-                moveX = -1
-                moveY = 0
-            elif event.key == pygame.K_UP:
-                moveY = -1
-                moveX = 0
+bg_music = pygame.mixer.Sound("assets/music_1.wav")
+bg_music.play(-1)
 
-    screen.fill(white)
-    # surface, color, (x,y), radius
-    # pygame.draw.circle(screen, green, (x, y), 50)
 
-    #scree, color, (x,y,length,breadth)
-    rect1 = pygame.draw.rect(screen, blue, (x, y, 50, 50))
-    rect2 = pygame.draw.rect(screen, red, (x2, y2, 50, 50))
-    x += moveX
-    # y += 1
-    y += moveY
+def homeScreen():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    mainScreen()
 
-    if rect1.colliderect(rect2):
-        x2 = random.randint(0, 950)
-        y2 = random.randint(0, 450)
+        # screen.fill(white)
 
-    # if y > 450:
-    #     # y -= 1
-    #     moveY = -1
-    # elif x > 950:
-    #     moveX = -1
-    # elif x < 50:
-    #     moveX = 1
-    # elif y < 50:
-    #     moveY = 1
+        snake_bg = pygame.image.load("assets/snake_bg.png")
+        screen.blit(snake_bg, (0, 0))
 
-    # if x < 0:
-    #     moveX = 1
-    # elif y < 0:
-    #     moveY = 1
-    # elif x > 900:
-    #     moveX = -1
-    # elif y > 400:
-    #     moveY = -1
+        msg = "Press SPACE to Start Game"
+        font = pygame.font.SysFont(None, 60)
+        text = font.render(msg, True, red)
+        screen.blit(text, (180, 450))
+        pygame.display.update()
 
-    if x > 1000:
-        x = -50
-    elif y > 500:
-        y = -50
-    elif x < -50:
-        x = 1000
-    elif y < -50:
-        y = 500
 
-    pygame.display.update()
+def mainScreen():
+
+    x = 100
+    y = 100
+    moveX = 0
+    moveY = 0
+    x2 = random.randint(0, width - 50)
+    y2 = random.randint(0, height - 50)
+    collisionSound = pygame.mixer.Sound("assets/point.wav")
+    counter = 0
+
+    while True:
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                quit()
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    moveX = 5
+                    moveY = 0
+                elif event.key == pygame.K_DOWN:
+                    moveY = 5
+                    moveX = 0
+                elif event.key == pygame.K_LEFT:
+                    moveX = -5
+                    moveY = 0
+                elif event.key == pygame.K_UP:
+                    moveY = -5
+                    moveX = 0
+
+        screen.fill(white)
+
+        scoreText = f"Score : {counter}"
+        font = pygame.font.SysFont(None, 30)
+        scoreText = font.render(scoreText, True, red)
+        screen.blit(scoreText, (20, 20))
+        # surface, color, (x,y), radius
+        # pygame.draw.circle(screen, green, (x, y), 50)
+
+        #scree, color, (x,y,length,breadth)
+        rect1 = pygame.draw.rect(screen, blue, (x, y, 50, 50))
+        rect2 = pygame.draw.rect(screen, red, (x2, y2, 50, 50))
+        x += moveX
+        # y += 1
+        y += moveY
+
+        if rect1.colliderect(rect2):
+            x2 = random.randint(0, width - 50)
+            y2 = random.randint(0, height - 50)
+            collisionSound.play()
+            counter += 1
+            # width += 50
+
+        # if y > 450:
+        #     # y -= 1
+        #     moveY = -1
+        # elif x > 950:
+        #     moveX = -1
+        # elif x < 50:
+        #     moveX = 1
+        # elif y < 50:
+        #     moveY = 1
+
+        # if x < 0:
+        #     moveX = 1
+        # elif y < 0:
+        #     moveY = 1
+        # elif x > 900:
+        #     moveX = -1
+        # elif y > 400:
+        #     moveY = -1
+
+        if x > width:
+            x = -50
+        elif y > height:
+            y = -50
+        elif x < -50:
+            x = width
+        elif y < -50:
+            y = height
+
+        pygame.display.update()
+
+
+homeScreen()
